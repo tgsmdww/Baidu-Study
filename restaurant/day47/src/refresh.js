128a4572c5a83f9269c1ff8a2046cd94de261901
tgsmdww <389007832@qq.com>
2018-08-07 16:45:03 +0800

    Delete refresh.js

--- restaurant/day47/src/refresh.js
+++
@@ -1,8 +0,0 @@
-    function refreshcash(cash){//刷新现金
-        ifeRestaurant.cash = cash;
-        rescash.innerText = cash;
-    }
-
-    function refreshcustomerstatus(customer){
-        customerstatus.innerHTML = `状态： ${customer.status}`;
-    }
